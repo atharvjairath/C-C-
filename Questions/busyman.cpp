3465868fd7882df8d3d69366a5afcc5c76c6c3d5
atharvjairath <atharv.jairath@gmail.com>
2021-08-18 17:20:48 +0530

    Adding New folder for Questions

---
+++ Questions/busyman.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// to sort by end time
+bool sortbysec(const pair<int,int> &a,
+               const pair<int,int> &b)
+{
+        return (a.second < b.second);
+}
+
+int solve(vector<pair<int,int> > &task, int n){
+        int ans =1;
+        sort(task.begin(), task.end(), sortbysec);
+        for (int i=1; i<n; i++) {
+                if(task[i].first >= task[i-1].second) {
+                        cout<<"S"<<task[i].first<<" "<<"e"<<task[i-1].second<<endl;
+                        ans++;
+                }
+        }
+        return ans;
+}
+int main(){
+        int t;
+        cin>>t;
+        while(t--) {
+                int n,s,e;
+                cin>>n;
+                vector<pair<int,int> >task;
+                for(int i=0; i<n; i++) {
+                        cin>>s>>e;
+                        task.push_back(make_pair(s,e));
+                }
+                cout<<t<<" "<<solve(task,n);
+
+
+        }
+
+        return 0;
+}
--- trie.cpp
+++ trie.cpp
@@ -2,93 +2,92 @@
 using namespace std;
 
 
-class Node{
+class Node {
 
-  public:
-    char data;
-    unordered_map<char,Node*>children;
-    bool terminal;
+public:
+char data;
+unordered_map<char,Node*>children;
+bool terminal;
 
-    Node(char d){
-      data = d;
-      terminal = false;
-  }
+Node(char d){
+        data = d;
+        terminal = false;
+}
 
 };
 
-class Trie{
+class Trie {
 
-  Node* root;
-  int cnt;
+Node* root;
+int cnt;
 
-  public:
-    Trie(){
-      root = new Node('\0');
-      cnt = 0;
-    }
+public:
+Trie(){
+        root = new Node('\0');
+        cnt = 0;
+}
 
-    //insert data in trie
-    void insert(char*w){
+//insert data in trie
+void insert(char*w){
 
-      Node* temp = root;
-      // iterate over the word
-      for(int i =0;w[i]!='\0';i++){
-        char ch = w[i];
-        // if the ch already exist then attach with temp
-        if(temp->children.count(ch)){
-          temp = temp->children[ch];
-        }
+        Node* temp = root;
+        // iterate over the word
+        for(int i =0; w[i]!='\0'; i++) {
+                char ch = w[i];
+                // if the ch already exist then attach with temp
+                if(temp->children.count(ch)) {
+                        temp = temp->children[ch];
+                }
 
-        // if new ch
-        else{
-          Node* n = new Node(ch);
-          temp->children[ch] = n;
-          temp = n;
-        }
-
-      }
-      temp->terminal = true;
-
-    }
-
-    // find function
-    bool find(char* w){
-      Node* temp = root;
-      // iterate over the word
-      for(int i =0;w[i]!='\0';i++){
-        char ch = w[i];
-        if(temp->children.count(ch)==0){
-          return false;
-        }
-        else{
-          temp->children[ch];
+                // if new ch
+                else{
+                        Node* n = new Node(ch);
+                        temp->children[ch] = n;
+                        temp = n;
+                }
 
         }
-        // check if its the terminal node or not
-        // if yes then the word exist
-      }
-      return temp->terminal;
+        temp->terminal = true;
 
-    }
+}
+
+// find function
+bool find(char* w){
+        Node* temp = root;
+        // iterate over the word
+        for(int i =0; w[i]!='\0'; i++) {
+                char ch = w[i];
+                if(temp->children.count(ch)==0) {
+                        return false;
+                }
+                else{
+                        temp->children[ch];
+
+                }
+                // check if its the terminal node or not
+                // if yes then the word exist
+        }
+        return temp->terminal;
+
+}
 
 };
 
 int main(){
 
-  Trie t;
-  char words[][10] = {"a","hello","not","news","apple"};
-  char w[10];
-  cin>>w;
-  for(int i =0;i<5;i++){
-    t.insert(words[i]);
-  }
+        Trie t;
+        char words[][10] = {"a","hello","not","news","apple"};
+        char w[10];
+        for(int i =0; i<5; i++) {
+                t.insert(words[i]);
+        }
+        cin>>w;
+        if(t.find(w)) {
+                cout<<"Word is present";
+        }
+        else{
+                cout<<"absent";
+        }
 
-  if(t.find(w)){
-    cout<<"Word is present";
-  }
-  else{
-    cout<<"absent";
-  }
-
-  return 0;
+        return 0;
 }
